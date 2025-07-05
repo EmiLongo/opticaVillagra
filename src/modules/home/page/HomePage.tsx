78a45c7e76d7cd1e36cefe92dd8879526ce970a5
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-07-05 13:36:55 -0300

    md HomePage - se agrega los componentes de sections y destacados

--- src/modules/home/page/HomePage.tsx
+++ src/modules/home/page/HomePage.tsx
@@ -1,10 +1,14 @@
 // src/modules/home/page/HomePage.tsx
 import { Hero } from "../components/Hero";
+import { Highlights } from "../components/Highlights";
+import { Sections } from "../components/Sections";
 
 export const HomePage = () => {
   return (
     <>
       <Hero />
+      <Sections />
+      <Highlights />
     </>
   );
 };
