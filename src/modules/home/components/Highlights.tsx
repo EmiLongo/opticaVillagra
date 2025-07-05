a906d53ee894251760bbc545f0e08d9e2762d5b1
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-07-05 17:01:56 -0300

    md Highlights - se genera una interface basica para productos, se acomodan estilos

--- src/modules/home/components/Highlights.tsx
+++ src/modules/home/components/Highlights.tsx
@@ -1,25 +1,50 @@
-import { ProductCard } from "@/shared/components/ProductCard"
-import { Title2 } from "@/theme/textStyles"
+// src/modules/home/components/Highlights.tsx
+import { ProductCard } from "@shared/components/ProductCard"
+
 import { Box } from "@mui/material"
 import React from "react"
 import productImageA from '@img/card/media.webp'
 import productImageB from '@img/card/media.png'
+import { SectionHeading } from "@/theme/textStyles"
 
-const products = [{
-  name: 'Anteojos de Sol',
-  discount: 50,
+export interface IProduct {
+  name: string
+  discount: number
+  price: number
+  priceDiscount: number
+  priceTransfer: number
+  plan: string
+  imageA: string
+  imageB: string
+}
+
+const products: IProduct[] = [{
+  name: 'Anteojos de sol Vulk Worthwile C2 Polarizado',
+  discount: 20,
+  price: 100000,
+  priceDiscount: 80000,
+  priceTransfer: 72000,
+  plan: "6 x $18.150,00 sin interés",
   imageA: productImageA,
   imageB: productImageB,
 },
 {
   name: 'Anteojos de Sol',
-  discount: 50,
+  discount: 20,
+  price: 100000,
+  priceDiscount: 80000,
+  priceTransfer: 72000,
+  plan: "6 x $18.150,00 sin interés",
   imageA: productImageA,
   imageB: productImageB,
 },
 {
   name: 'Anteojos de Sol',
-  discount: 50,
+  discount: 20,
+  price: 100000,
+  priceDiscount: 80000,
+  priceTransfer: 72000,
+  plan: "6 x $18.150,00 sin interés",
   imageA: productImageA,
   imageB: productImageB,
 },
@@ -28,18 +53,22 @@ const products = [{
 export const Highlights: React.FC = () => {
   return (
     <Box sx={{
-      maxWidth: "1280px",
-      marginX: "auto",
-      paddingX: {xs: "1rem", md: "3rem"},
+      marginBottom: "5rem",
+      paddingX: {xs: "1rem", md: "3rem", lg: "4rem"},
     }}>
-      <Title2 sx={{
+      <SectionHeading
+      id= "section-highlights"
+      sx={{
         width: "100%",
-        textAlign: "center"
-      }}>Destacados</Title2>
+        textAlign: "center",
+      }}
+      >
+        Destacados
+      </SectionHeading>
       <Box sx={{
         display: "grid",
         gridTemplateColumns: {xs: "1fr", sm: "repeat(2, 1fr)", md: "repeat(3, 1fr)", lg: "repeat(4, 1fr)"},
-        gap: "1rem",
+        gap: {xs: "1rem", md: "1.5rem", lg: "2rem"},
       }}>
         {products.map((product) => (
           <ProductCard product={product} />
