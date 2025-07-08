0ed696701e6a83049a82ae26bfdb55b6d5ad9d9e
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-07-08 17:19:09 -0300

    fix useLogin - se usan nuevos nombres de interfaces

--- src/shared/hooks/api/auth/useLogin.ts
+++ src/shared/hooks/api/auth/useLogin.ts
@@ -1,16 +1,16 @@
 // src/hooks/useLogin.ts
 import { useState, useCallback } from 'react';
-import { login, LoginData, LoginResponse } from '@/api/authApi';
+import { ILoginData, ILoginResponse, login, } from '@/api/authApi';
 import { toast } from 'react-toastify';
 import { useUserStore } from '@/store/useUserStore';
 
 
 const useLogin = () => {
   const [isLoading, setIsLoading] = useState(false);
-  const [user, setUser] = useState<LoginResponse | null>(null);
+  const [user, setUser] = useState<ILoginResponse | null>(null);
   const [error, setError] = useState<string | null>(null);
 
-  const handleLogin = useCallback(async (loginData: LoginData) => {
+  const handleLogin = useCallback(async (loginData: ILoginData) => {
     setIsLoading(true);
     setError(null);
     try {
