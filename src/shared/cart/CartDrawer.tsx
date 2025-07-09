8b887aea7ff62982d2047f95469dafa1731686f7
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-07-08 21:02:17 -0300

    md CartDrawer - se completa el drawer sin logica de carrito

--- src/shared/cart/CartDrawer.tsx
+++ src/shared/cart/CartDrawer.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { Box, Drawer } from "@mui/material";
+import { Box, Divider, Drawer } from "@mui/material";
 import CloseIcon from '@mui/icons-material/Close';
-import { BodyS, Heading2 } from "@/theme/textStyles";
+import { BodyS, Heading2, Heading4 } from "@/theme/textStyles";
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import { ICart } from "./types";
 import { CartDrawerItem } from "./CartDrawerItem";
+import { greyColor } from "@/theme/theme";
+import { numberToPrice } from "../utils/convertNumberToPrice";
+import { ColorButton } from "../components/buttons/ColorButton";
+import { useNavigate } from "react-router-dom";
 
 interface ICartDrawerProps {
   openCartDrawer: boolean
@@ -34,8 +38,8 @@ const mockCart: ICart = {
         isValid: true,
         title: 'Auriculares Bluetooth',
         description: 'Auriculares inalámbricos con cancelación de ruido',
-        urlPhoto: 'https://example.com/images/auriculares.jpg',
-        price: 1999,
+        urlPhoto: 'https://mundoadaptogenos.netlify.app/assets/img/thumbnail/frasco_melena_de_leon.jpg',
+        price: 91999,
         priceDiscount: 1999,
         discount: 20,
         priceTransfer: 1799,
@@ -55,8 +59,113 @@ const mockCart: ICart = {
         isValid: true,
         title: 'Teclado Mecánico',
         description: 'Teclado mecánico RGB con switches rojos',
-        urlPhoto: 'https://example.com/images/teclado.jpg',
-        price: 3499,
+        urlPhoto: 'https://mundoadaptogenos.netlify.app/assets/img/thumbnail/frasco_reishi.jpg',
+        price: 93499,
+        priceDiscount: 3499,
+        discount: 300,
+        priceTransfer: 3199,
+        plan: "3299",
+        createdAt: new Date().toISOString(),
+      },
+    },
+    {
+      id: 103,
+      cartId: 1,
+      articuloId: 502,
+      quantity: 1,
+      addedAt: new Date().toISOString(),
+      product: {
+        id: 502,
+        sectionId: 12,
+        isValid: true,
+        title: 'Teclado Mecánico',
+        description: 'Teclado mecánico RGB con switches rojos',
+        urlPhoto: 'https://mundoadaptogenos.netlify.app/assets/img/thumbnail/frasco_reishi.jpg',
+        price: 93499,
+        priceDiscount: 3499,
+        discount: 300,
+        priceTransfer: 3199,
+        plan: "3299",
+        createdAt: new Date().toISOString(),
+      },
+    },
+    {
+      id: 103,
+      cartId: 1,
+      articuloId: 502,
+      quantity: 1,
+      addedAt: new Date().toISOString(),
+      product: {
+        id: 502,
+        sectionId: 12,
+        isValid: true,
+        title: 'Teclado Mecánico',
+        description: 'Teclado mecánico RGB con switches rojos',
+        urlPhoto: 'https://mundoadaptogenos.netlify.app/assets/img/thumbnail/frasco_reishi.jpg',
+        price: 93499,
+        priceDiscount: 3499,
+        discount: 300,
+        priceTransfer: 3199,
+        plan: "3299",
+        createdAt: new Date().toISOString(),
+      },
+    },
+    {
+      id: 105,
+      cartId: 1,
+      articuloId: 502,
+      quantity: 1,
+      addedAt: new Date().toISOString(),
+      product: {
+        id: 502,
+        sectionId: 12,
+        isValid: true,
+        title: 'Teclado Mecánico',
+        description: 'Teclado mecánico RGB con switches rojos',
+        urlPhoto: 'https://mundoadaptogenos.netlify.app/assets/img/thumbnail/frasco_reishi.jpg',
+        price: 93499,
+        priceDiscount: 3499,
+        discount: 300,
+        priceTransfer: 3199,
+        plan: "3299",
+        createdAt: new Date().toISOString(),
+      },
+    },
+    {
+      id: 106,
+      cartId: 1,
+      articuloId: 502,
+      quantity: 1,
+      addedAt: new Date().toISOString(),
+      product: {
+        id: 502,
+        sectionId: 12,
+        isValid: true,
+        title: 'Teclado Mecánico',
+        description: 'Teclado mecánico RGB con switches rojos',
+        urlPhoto: 'https://mundoadaptogenos.netlify.app/assets/img/thumbnail/frasco_reishi.jpg',
+        price: 93499,
+        priceDiscount: 3499,
+        discount: 300,
+        priceTransfer: 3199,
+        plan: "3299",
+        createdAt: new Date().toISOString(),
+      },
+    },
+    {
+      id: 107,
+      cartId: 1,
+      articuloId: 502,
+      quantity: 1,
+      addedAt: new Date().toISOString(),
+      product: {
+        id: 502,
+        sectionId: 12,
+        isValid: true,
+        title: 'Teclado Mecánico',
+        description: 'Teclado mecánico RGB con switches rojos',
+        urlPhoto: 'https://mundoadaptogenos.netlify.app/assets/img/thumbnail/frasco_reishi.jpg',
+        price: 93499,
         priceDiscount: 3499,
         discount: 300,
         priceTransfer: 3199,
@@ -73,6 +182,13 @@ const mockCart: ICart = {
 
 export const CartDrawer: React.FC<ICartDrawerProps> = ({openCartDrawer = false, closeCartDrawer = () => {}}) => {
   console.log(mockCart)
+  const navigate = useNavigate();
+  const cartSubtotal= 100000
+  const cartTotal=200000
+  const handleStartCheckout = () => {
+    closeCartDrawer();
+    navigate("./cart");
+  }
   return(
     <Drawer
       variant="temporary"
@@ -86,25 +202,77 @@ export const CartDrawer: React.FC<ICartDrawerProps> = ({openCartDrawer = false, 
       <Box sx={{
         position: 'relative', 
         height: '100%', 
-        width: {xs: "100vw", md:"30vw"}, 
-        minWidth: {xs: "unset", md: "420px"}, 
-        padding: "12px" 
+        width: "min(100vw, 420px)", 
+        padding: "12px", 
+        display: "flex",
+        flexDirection:"column",
       }}>
+        {/* Boton cierre */}
         <Box
           sx={{ position: 'absolute', top: "1rem", right: "1rem" }}
           onClick={closeCartDrawer}
         >
           <CloseIcon />
         </Box>
-        <Heading2>Carrito de compras</Heading2>
-        <Box sx={{display: "flex", alignItems: "center", gap: "10px"}}>
-          <AccountCircleOutlinedIcon />
-          <BodyS>maildeejemplo@gmail.com</BodyS>
+
+        {/* Titulo */}
+        <Box>
+          <Heading2 sx={{marginBottom: "8px"}}>Carrito de compras</Heading2>
+          <Box sx={{
+            display: "flex", 
+            alignItems: "center", 
+            gap: "10px", 
+            marginBottom: "24px"
+          }}>
+            <AccountCircleOutlinedIcon sx={{color: greyColor[950]}}/>
+            <BodyS>maildeejemplo@gmail.com</BodyS>
+          </Box>
         </Box>
-        {mockCart.cartItems.map((cartItem, index)=>(
-          <CartDrawerItem cartItem={cartItem} index={index} />
-        ))
-        }
+
+        {/* Productos de carrito */}
+        <Box sx={{flexGrow:1, overflowY: "auto"}}>
+          {mockCart.cartItems.map((cartItem, index)=>(
+            <CartDrawerItem cartItem={cartItem} index={index} />
+          ))}
+        </Box>
+
+        {/* Subtotal Carrito */}
+        <Box sx={{display: "flex", flexDirection: "column", gap: "16px"}}>
+          <Box sx={{display: "flex", flexDirection: "column", gap: "8px"}}>
+            <Divider sx={{height: "8px", backgroundColor: "secondary.dark", borderRadius: "15px"}} />
+            <BodyS sx={{width: "100%", textAlign: "center", color: "secondary.dark"}}>¡Genial! Tenés envío gratis.</BodyS>
+          </Box>
+          
+          <Box sx={{display: "flex", flexDirection: "column", gap: "8px"}}>
+            <Box sx={{display: "flex", alignItems:"center", justifyContent:"space-between" }}>
+              <Heading4>Subtotal (sin envío):</Heading4>
+              <Heading4>{numberToPrice(cartSubtotal)}</Heading4>
+            </Box>
+            <Box sx={{display: "flex", alignItems:"center", justifyContent:"space-between" }}>
+              <Heading4>Envío:</Heading4>
+              <Heading4 sx={{color: "secondary.dark"}}>GRATIS</Heading4>
+            </Box>
+          </Box>  
+          <Box sx={{display: "flex", flexDirection: "column", gap: "8px"}}>
+            <Box sx={{display: "flex", alignItems:"center", justifyContent:"space-between"}}>
+              <Heading2>TOTAL:</Heading2>
+              <Heading2>{numberToPrice(cartTotal)}</Heading2>
+            </Box>
+            <Box sx={{display: "flex", alignItems:"center", justifyContent:"end" }}>
+              <BodyS sx={{color: "secondary.dark"}}>{`o ${numberToPrice(cartTotal * 0.9)} con Transferencia o depósito`}</BodyS>
+            </Box>
+          </Box>
+          <ColorButton
+          id="bt-cart-drawer-begin"
+          type="blueButton"
+          onClick={handleStartCheckout}
+          text="Iniciar compra"
+          fetchingText=""
+          isFetching={false}
+          disabled={false}
+          sx={{width: "100%"}}
+          />
+        </Box>
       </Box>
     </Drawer>
   )
