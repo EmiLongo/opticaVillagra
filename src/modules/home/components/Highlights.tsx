31337121485ca9709b0db5a00d6df2d7bccfd1b2
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-07-10 18:20:27 -0300

    md HighLights - se refactorizan los productos y se usa el nuevo catalogo

--- src/modules/home/components/Highlights.tsx
+++ src/modules/home/components/Highlights.tsx
@@ -1,98 +1,10 @@
 // src/modules/home/components/Highlights.tsx
-import { ProductCard } from "@/shared/cart/ProductCard"
-
 import { Box } from "@mui/material"
 import React from "react"
-import productImageA from '@img/card/media.webp'
-import { SectionHeading } from "@/theme/textStyles"
-import { IProduct } from "@/shared/components/types"
+import { SectionHeading } from "@theme/textStyles"
+import { catalogue } from "@shared/Layout/utils/catalogue"
+import { ProductCard } from "@shared/cart/ProductCard"
 
-
-const products: IProduct[] = [{
-  id: 123,
-  sectionId: 3,
-  title: 'Anteojos de sol Vulk Worthwile C2 Polarizado',
-  discount: 20,
-  price: 100000,
-  priceDiscount: 80000,
-  priceTransfer: 72000,
-  plan: "6 x $18.150,00 sin interés",
-  urlPhoto: productImageA,
-  isValid: true,
-},
-{
-  id: 124,
-  sectionId: 3,
-  title: 'Anteojos de Sol',
-  discount: 20,
-  price: 100000,
-  priceDiscount: 80000,
-  priceTransfer: 72000,
-  plan: "6 x $18.150,00 sin interés",
-  urlPhoto: productImageA,
-  isValid: true,
-},
-{
-  id: 125,
-  sectionId: 3,
-  title: 'Anteojos de Sol',
-  discount: 20,
-  price: 100000,
-  priceDiscount: 80000,
-  priceTransfer: 72000,
-  plan: "6 x $18.150,00 sin interés",
-  urlPhoto: productImageA,
-  isValid: true,
-},
-{
-  id: 126,
-  sectionId: 3,
-  title: 'Anteojos de Sol',
-  discount: 20,
-  price: 100000,
-  priceDiscount: 80000,
-  priceTransfer: 72000,
-  plan: "6 x $18.150,00 sin interés",
-  urlPhoto: productImageA,
-  isValid: true,
-},
-{
-  id: 127,
-  sectionId: 3,
-  title: 'Anteojos de Sol',
-  discount: 20,
-  price: 100000,
-  priceDiscount: 80000,
-  priceTransfer: 72000,
-  plan: "6 x $18.150,00 sin interés",
-  urlPhoto: productImageA,
-  isValid: true,
-},
-{
-  id: 128,
-  sectionId: 3,
-  title: 'Anteojos de Sol',
-  discount: 20,
-  price: 100000,
-  priceDiscount: 80000,
-  priceTransfer: 72000,
-  plan: "6 x $18.150,00 sin interés",
-  urlPhoto: productImageA,
-  isValid: true,
-},
-{
-  id: 129,
-  sectionId: 3,
-  title: 'Anteojos de Sol',
-  discount: 20,
-  price: 100000,
-  priceDiscount: 80000,
-  priceTransfer: 72000,
-  plan: "6 x $18.150,00 sin interés",
-  urlPhoto: productImageA,
-  isValid: true,
-},
-]
 
 export const Highlights: React.FC = () => {
   return (
@@ -114,7 +26,7 @@ export const Highlights: React.FC = () => {
         gridTemplateColumns: {xs: "1fr", sm: "repeat(2, 1fr)", md: "repeat(3, 1fr)", lg: "repeat(4, 1fr)", xl: "repeat(5, 1fr)"},
         gap: {xs: "1rem", md: "1.5rem", lg: "2rem"},
       }}>
-        {products.map((product, index) => (
+        {catalogue.map((product, index) => (
           <ProductCard product={product} index={index} />
         ))}
       </Box>
