2c12c9f3eb4ea414ee53dd1917cc3106eb7226ac
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-07-10 18:19:28 -0300

    md CartButton, el estado que tenia antes se le pasan por props

--- src/shared/cart/CartButton.tsx
+++ src/shared/cart/CartButton.tsx
@@ -1,17 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import { greyColor } from "@/theme/theme";
 import { Badge, IconButton } from "@mui/material";
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import { CartDrawer } from "./CartDrawer";
 
-export const CartButton: React.FC = () => {
-  const [openCartDrawer, setOpenCartDrawer] = useState<boolean>(false)
-  const closeCartDrawer = () => {
-    setOpenCartDrawer(false)
-  }
-  const handleCartButton = () => {
-    setOpenCartDrawer(!openCartDrawer)
-  }
+interface ICartButton {
+  openCartDrawer: boolean;
+  closeCartDrawer: ()=>void;
+  handleCartButton: ()=>void;
+}
+export const CartButton: React.FC<ICartButton> = ({openCartDrawer, closeCartDrawer, handleCartButton}) => {
+
   return (
     <>
     <IconButton onClick={handleCartButton} sx={{border: {xs: "none", md:`1px solid ${greyColor[950]}`,} }}>
